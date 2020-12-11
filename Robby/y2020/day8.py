a80828d54579b737022d67e832606ed61770221c
burningshed <BurningShed@gmail.com>
2020-12-11 12:21:22 -0500

    moved my windows files to repo

---
+++ Robby/y2020/day8.py
@@ -0,0 +1,19 @@
+class hh_func():
+    def __init__(self, name, op, tar)
+class aoc_comp_2020():
+    self.com_dict = {
+       "nop" : self.nop,
+       "acc" : self.acc,
+       "jmp" : self.jmp
+    def nop(self, val):
+        pass
+    def acc(self, val):
+        pass
+    def jmp(self, val):
+        pass
+}
+class handheld():
+    def __init__(self, raw_ins_set):
+        if type(raw_ins_set) == str:
+            s_ins_set = raw_ins_set
+
